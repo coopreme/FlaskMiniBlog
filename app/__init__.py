4bdbdcca3c898386e396ecb9c5dbfa0daa24055c
coopreme <me@coopreme.com>
2021-03-27 19:03:02 -0400

    datetime fixes but some l10n and l8s

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel
 import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
@@ -18,6 +20,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+babel = Babel(app)
 
 from app import routes, models, errors  # noqa
 
@@ -48,3 +52,7 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info("MiniBlog Startup")
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages;.best_match(app.config['LANGUAGES'])
--- config.py
+++ config.py
@@ -14,3 +14,4 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
     POSTS_PER_PAGE = 20
+    LANGUAGES = ['en', 'it']
