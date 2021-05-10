4bafdc1357a941e8fdeb968d263ed4ebdf81deea
coopreme <me@coopreme.com>
2021-05-10 10:57:54 -0400

    adding popover for user info and docker compose

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv
+import json
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env.'))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-do-not-know'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -14,8 +15,8 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = json.loads(os.environ['ADMIN_EMAILS'])
     POSTS_PER_PAGE = 20
     LANGUAGES = ['en', 'it']
-    TRANS_KEY = os.environ.get('TRANS_KEY')
+    TRANS_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
