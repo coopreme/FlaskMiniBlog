6872604f3452b1a72c220355f88186893590bff3
coopreme <me@coopreme.com>
2021-05-12 17:01:27 -0400

    a major fix to the compose steps

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = json.loads(os.environ.get['ADMIN_EMAILS'])
+    ADMINS = os.environ.get('ADMIN_EMAIL')
     POSTS_PER_PAGE = 20
     LANGUAGES = ['en', 'it']
     TRANS_KEY = os.environ.get('MS_TRANSLATOR_KEY')
